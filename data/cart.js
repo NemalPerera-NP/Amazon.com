bdd4b59447fc8ac2fd9795afc94bb08c7e0e757e
NemalPerera-NP <nemalperera1999@gmail.com>
2024-08-24 13:03:47 +0530

    create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,18 +1,24 @@
-let cart = JSON.parse(localStorage.getItem("cart"));
+let cart;
 
-if (!cart) {
-  cart = [
-    {
-      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-      quantity: 1,
-      deliveryOptionId: "1",
-    },
-    {
-      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-      quantity: 2,
-      deliveryOptionId: "2",
-    },
-  ];
+loadFromeStorage();
+
+function loadFromeStorage() {
+  cart = JSON.parse(localStorage.getItem("cart"));
+
+  if (!cart) {
+    cart = [
+      {
+        productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        quantity: 1,
+        deliveryOptionId: "1",
+      },
+      {
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2,
+        deliveryOptionId: "2",
+      },
+    ];
+  }
 }
 
 function saveToLocalStorage() {
@@ -72,4 +78,5 @@ export {
   removeFromCart,
   saveToLocalStorage,
   updateDeliveryOption,
+  loadFromeStorage,
 };
